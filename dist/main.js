aa7f31d28be79229e1a193f7533315ed7421ab3c
Kaen Pawanachai <pawaox4th@gmail.com>
2023-03-19 20:16:12 +0700

    refactor🔨: change versioning and build.

--- dist/main.js
+++ dist/main.js
@@ -1,77 +1,91 @@
-#!/usr/bin/env node
-import { intro, note, outro, select, spinner, text } from "@clack/prompts"
-import { exec, spawnSync } from "node:child_process"
-async function main() {
-  console.clear()
-  intro("Welcome to the commit-cli demo! .")
-  const selected = await select({
-    message: "Commit type.",
-    options: [
-      {
-        value: "chore🤖 :",
-        label: "chore🤖 :",
-      },
-      {
-        label: "ci🎡 :",
-        value: "ci🎡 :",
-      },
-      {
-        label: "docs✏️ :",
-        value: "docs✏️ :",
-      },
-      {
-        label: "feat✨ :",
-        value: "feat✨ :",
-      },
-      {
-        label: "fix🐛 :",
-        value: "fix🐛 :",
-      },
-      {
-        label: "perf🚀 :",
-        value: "perf🚀 :",
-      },
-      {
-        label: "refactor🔨 :",
-        value: "refactor🔨 :",
-      },
-      {
-        label: "revert🔀 :",
-        value: "revert🔀 :",
-      },
-      {
-        label: "style💄 :",
-        value: "style💄 :",
-      },
-      {
-        label: "test🧪 :",
-        value: "test🧪 :",
-      },
-      {
-        label: "other🍻 :",
-        value: "other🍻 :",
-      },
-      {
-        label: "release✅🎉 :",
-        value: "release✅🎉 :",
-      },
-    ],
-  })
-  const commitMsg = await text({
-    message: "Commit message ?",
-    validate: (value) => {
-      if (!value) return "Please enter a message."
+import * as p from "@clack/prompts";
+import { exec, spawnSync } from "node:child_process";
+const typeForCommit = [
+    {
+        value: "chore🤖:",
+        label: "chore🤖:",
     },
-  })
-  const dataCommit = `${selected}${commitMsg.toString()}`
-  note(dataCommit, "Commit message ...")
-  const s = spinner()
-  s.start("Commit ...")
-  await spawnSync("git", ["add", "."])
-  await exec(`git commit -m "${dataCommit}"`)
-  // await setTimeout(3000)
-  s.stop("")
-  outro("Commit success !!!!")
+    {
+        label: "ci🎡:",
+        value: "ci🎡:",
+    },
+    {
+        label: "docs✏️:",
+        value: "docs✏️:",
+    },
+    {
+        label: "feat✨:",
+        value: "feat✨:",
+    },
+    {
+        label: "fix🐛:",
+        value: "fix🐛:",
+    },
+    {
+        label: "perf🚀:",
+        value: "perf🚀:",
+    },
+    {
+        label: "refactor🔨:",
+        value: "refactor🔨:",
+    },
+    {
+        label: "revert🔀:",
+        value: "revert🔀:",
+    },
+    {
+        label: "style💄:",
+        value: "style💄:",
+    },
+    {
+        label: "test🧪:",
+        value: "test🧪:",
+    },
+    {
+        label: "other🍻:",
+        value: "other🍻:",
+    },
+    {
+        label: "release✅🎉:",
+        value: "release✅🎉:",
+    },
+];
+async function main() {
+    console.clear();
+    p.intro(`Welcome to the commit-cli !!!.`);
+    const commitWorkFlows = await p.group({
+        selectType() {
+            return p.select({
+                message: "Commit type.",
+                options: [...typeForCommit],
+            });
+        },
+        commitMsg: () => p.text({
+            message: "Commit message ?",
+            validate: (value) => {
+                if (!value)
+                    return "Please enter a message.";
+            },
+        }),
+        display: ({ results }) => {
+            p.note(`${results.selectType} ${results.commitMsg} ...`);
+        },
+        process: async (data) => {
+            const s = p.spinner();
+            s.start();
+            const dataCommit = `${data.results.selectType} ${data.results.commitMsg}`;
+            await spawnSync("git", ["add", "."]);
+            await exec(`git commit -m "${dataCommit}"`);
+            s.stop();
+        },
+        success: () => {
+            p.outro("Commit success !!!!");
+            process.exit(0);
+        },
+    });
 }
-main()
+main().catch(() => {
+    p.cancel("Cancel commit ");
+    process.exit(0);
+});
 //# sourceMappingURL=main.js.map
